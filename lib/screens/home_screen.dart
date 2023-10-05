956ab78eacf45877945527ef08d1be3f9f84033b
chaeju <cown4288@gmail.com>
2023-10-06 02:59:27 +0900

    profile.dart 파일 import

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,6 +6,7 @@ import 'package:table_calendar/table_calendar.dart';
 import 'game_screen.dart';
 import 'signin_screen.dart';
 import '../class/task.dart';
+import 'profile.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -15,158 +16,149 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   FirebaseAuth auth = FirebaseAuth.instance;
-  final _textController=TextEditingController();
-  List<Task>tasks=[];
-  DateTime selectedDay=DateTime.now();
+  final _textController = TextEditingController();
+  List<Task> tasks = [];
+  DateTime selectedDay = DateTime.now();
 
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset : false,
-        appBar: AppBar(
-          //앱바 구성
-          title: const Text('영단어 사전(test)'),
-          centerTitle: true, //타이틀 중앙 위치
-          elevation: 0.0, //입체감 없애기
-          backgroundColor: Colors.purple,
-          actions: [
-            IconButton(
-              onPressed: () {
-                print('사람');
-              },
-              icon: const Icon(Icons.person),
-            )
-          ],
-        ),
-        drawer: Drawer(
-            child: ListView(
-              
-          children: [
-            UserAccountsDrawerHeader(
-              //유저 헤더
-              currentAccountPicture: const CircleAvatar(
-                backgroundColor: Colors.white,
-              ),
-              accountName: Text(auth.currentUser!.displayName.toString()),
-              accountEmail: Text(auth.currentUser!.email.toString()),
-              onDetailsPressed: () {}, // 디테일
-              decoration: BoxDecoration(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        //앱바 구성
+        title: const Text('영단어 사전(test)'),
+        centerTitle: true, //타이틀 중앙 위치
+        elevation: 0.0, //입체감 없애기
+        backgroundColor: Colors.purple,
+        actions: [
+          IconButton(
+            onPressed: () {
+              print('사람');
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const Profile()));
+            },
+            icon: const Icon(Icons.person),
+          )
+        ],
+      ),
+      drawer: Drawer(
+          child: ListView(
+        children: [
+          UserAccountsDrawerHeader(
+            //유저 헤더
+            currentAccountPicture: const CircleAvatar(
+              backgroundColor: Colors.white,
+            ),
+            accountName: Text(auth.currentUser!.displayName.toString()),
+            accountEmail: Text(auth.currentUser!.email.toString()),
+            onDetailsPressed: () {}, // 디테일
+            decoration: BoxDecoration(
                 color: Colors.purple[200],
                 borderRadius: const BorderRadius.only(
-                  bottomLeft: Radius.circular(15),
-                  bottomRight: Radius.circular(15)
-                )),
-              ),
-               ListTile(
-                leading: const Icon(Icons.view_list),
-                title: const Text('단어장'),
-                iconColor: Colors.purple,
-                onTap: () {print('단어장');},
-              
-              ),
-               
-               ListTile(
-                leading: const Icon(Icons.sports_esports),
-                title: const Text('게임'),
-                iconColor:Colors.purple,
-                onTap: () {
-                print('게임');
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const GameScreen()));
-              },
-            ),
-               ListTile(
-                leading: const Icon(Icons.shopping_cart),
-                title: const Text('상점'),
-                iconColor: Colors.purple,
-                onTap: () {print('상점');},
-                
-              ),
-               ListTile(
-                leading: const Icon(Icons.logout),
-                title: const Text('로그아웃'),
-                iconColor: Colors.purple,
-                onTap: () {
-                  FirebaseAuth.instance.signOut().then((value) {
-                    print("Signed Out");
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const SignInScreen()));
-                  });
-                }),
-          ],
-        )
-      ),
-    body:Center(
-        child: Column(
-       // mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-           
+                    bottomLeft: Radius.circular(15),
+                    bottomRight: Radius.circular(15))),
+          ),
+          ListTile(
+            leading: const Icon(Icons.view_list),
+            title: const Text('단어장'),
+            iconColor: Colors.purple,
+            onTap: () {
+              print('단어장');
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.sports_esports),
+            title: const Text('게임'),
+            iconColor: Colors.purple,
+            onTap: () {
+              print('게임');
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const GameScreen()));
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.shopping_cart),
+            title: const Text('상점'),
+            iconColor: Colors.purple,
+            onTap: () {
+              print('상점');
+            },
+          ),
+          ListTile(
+              leading: const Icon(Icons.logout),
+              title: const Text('로그아웃'),
+              iconColor: Colors.purple,
+              onTap: () {
+                FirebaseAuth.instance.signOut().then((value) {
+                  print("Signed Out");
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const SignInScreen()));
+                });
+              }),
+        ],
+      )),
+      body: Center(
+          child: Column(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              children: [
             //flex:5,
-             TableCalendar(
-               locale: 'ko_KR',
-               firstDay: DateTime.utc(2021, 10, 16),
-               lastDay: DateTime.utc(2030, 3, 14),
-               focusedDay: DateTime.now(),
-               onDaySelected: (DateTime selectedDay,DateTime focusedDay){
-                  setState(() {
-                    this.selectedDay=selectedDay;
-                    //this.focusedDay = focusedDay;
-                  });
-               },
-
-               headerStyle: const HeaderStyle(
+            TableCalendar(
+              locale: 'ko_KR',
+              firstDay: DateTime.utc(2021, 10, 16),
+              lastDay: DateTime.utc(2030, 3, 14),
+              focusedDay: DateTime.now(),
+              onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
+                setState(() {
+                  this.selectedDay = selectedDay;
+                  //this.focusedDay = focusedDay;
+                });
+              },
+              headerStyle: const HeaderStyle(
                 titleCentered: true,
                 // titleTextFormatter: (date, locale) =>
-          //    DateFormat.yMMMMd(locale).format(date),
-              formatButtonVisible: false,
-              titleTextStyle: TextStyle(
-            fontSize: 20.0,
-          ),
-              leftChevronIcon: Icon(
-            Icons.arrow_left,
-            size: 40.0,
-          ),
-          rightChevronIcon: Icon(
-            Icons.arrow_right,
-            size: 40.0,
-          ),
-               ),
-           
-           calendarStyle: CalendarStyle(
-             isTodayHighlighted: true,
-              selectedDecoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(color: Colors.deepPurple, width: 1.0),
-                  ),
-              selectedTextStyle: const TextStyle(
-                color: Colors.amber,
-  fontSize: 16.0,
+                //    DateFormat.yMMMMd(locale).format(date),
+                formatButtonVisible: false,
+                titleTextStyle: TextStyle(
+                  fontSize: 20.0,
+                ),
+                leftChevronIcon: Icon(
+                  Icons.arrow_left,
+                  size: 40.0,
+                ),
+                rightChevronIcon: Icon(
+                  Icons.arrow_right,
+                  size: 40.0,
+                ),
               ),
-              todayDecoration: BoxDecoration(
-          color: Colors.transparent,
-          shape: BoxShape.circle,
-          border: Border.all(color: Colors.purple,width: 1.5)
-        ),
-        todayTextStyle: const TextStyle(
-          fontWeight: FontWeight.bold,
-        ),
-           
+              calendarStyle: CalendarStyle(
+                isTodayHighlighted: true,
+                selectedDecoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.deepPurple, width: 1.0),
+                ),
+                selectedTextStyle: const TextStyle(
+                  color: Colors.amber,
+                  fontSize: 16.0,
+                ),
+                todayDecoration: BoxDecoration(
+                    color: Colors.transparent,
+                    shape: BoxShape.circle,
+                    border: Border.all(color: Colors.purple, width: 1.5)),
+                todayTextStyle: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
-      ),
-      Padding(  //선택한 날짜 표시
-        padding: const EdgeInsets.all(0.0),
-        child:Column(children: [
+            Padding(
+              //선택한 날짜 표시
+              padding: const EdgeInsets.all(0.0),
+              child: Column(children: [
                 Container(
-                  color: Colors.purple[50],
+                    color: Colors.purple[50],
                     padding: const EdgeInsets.fromLTRB(20, 10, 16, 0),
-                    
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
@@ -179,65 +171,62 @@ class _HomeScreenState extends State<HomeScreen> {
                               fontWeight: FontWeight.bold,
                               height: 2.0),
                         ),
-                        
                       ],
-                      )),
-        ]
-        ),
-        ),  
-       Padding( //텍스트박스
-            padding: const EdgeInsets.all(8.0),
-            
-            child: 
-             Row(
+                    )),
+              ]),
+            ),
+            Padding(
+              //텍스트박스
+              padding: const EdgeInsets.all(8.0),
+
+              child: Row(
                 children: [
-                  
                   Flexible(
-                     flex:1,
+                    flex: 1,
                     child: TextField(
                       controller: _textController,
                     ),
                   ),
                   ElevatedButton(
                     onPressed: () {
-                      if(_textController.text==''){
+                      if (_textController.text == '') {
                         return;
-                      }else{
-                       setState(() {
-                         var task=Task(_textController.text);
-                         tasks.add(task);
-                         _textController.clear();
-                       });
-                        }
-                      },
+                      } else {
+                        setState(() {
+                          var task = Task(_textController.text);
+                          tasks.add(task);
+                          _textController.clear();
+                        });
+                      }
+                    },
                     child: const Text("추가"),
                   )
                 ],
               ),
             ),
-      for(var i=0;i<tasks.length;i++)
-           Row(
-              children: [
-                Flexible(
-                  child: TextButton(
-                    style: TextButton.styleFrom(
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.zero),
+            for (var i = 0; i < tasks.length; i++)
+              Row(
+                children: [
+                  Flexible(
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.zero),
+                        ),
                       ),
-                    ),
-                    onPressed: () {},
-                    child:Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        children: [
-                          const Icon(Icons.check_box_outline_blank_rounded), 
-                          Text(tasks[i].work)
-                        ],
+                      onPressed: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            const Icon(Icons.check_box_outline_blank_rounded),
+                            Text(tasks[i].work)
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                ),
-                /*
+                  /*
                 TextButton(
                   onPressed: () {
                     setState(() {
@@ -249,20 +238,17 @@ class _HomeScreenState extends State<HomeScreen> {
                   child: const Text("수정"),
                 ),
                 */
-                TextButton(
-                  onPressed: () {
-                    setState(() {
-                      tasks.remove(tasks[i]);
-                    });
-                  },
-                  child: const Text("삭제"),
-                ),
-              ],
-            ),
-            
-         
-      ])),
-    
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        tasks.remove(tasks[i]);
+                      });
+                    },
+                    child: const Text("삭제"),
+                  ),
+                ],
+              ),
+          ])),
     );
   }
 }
---
+++ lib/screens/profile.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+
+class Profile extends StatefulWidget {
+  const Profile({Key? key}) : super(key: key);
+
+  @override
+  _ProfileState createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  XFile? _pickedFile;
+
+  @override
+  Widget build(BuildContext context) {
+    final _imageSize = MediaQuery.of(context).size.width / 4;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('프로필'),
+        backgroundColor: Colors.purple,
+        foregroundColor: Colors.white,
+      ),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 20,
+          ),
+          if (_pickedFile == null)
+            Container(
+              constraints: BoxConstraints(
+                minHeight: _imageSize,
+                minWidth: _imageSize,
+              ),
+              child: GestureDetector(
+                onTap: () {
+                  _showBottomeSheet();
+                },
+                child: Center(
+                  child: Icon(
+                    Icons.account_circle,
+                    size: _imageSize,
+                  ),
+                ),
+              ),
+            )
+          else
+            Container(
+                width: _imageSize,
+                height: _imageSize,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                      width: 2, color: Theme.of(context).colorScheme.primary),
+                  image: DecorationImage(
+                      image: FileImage(File(_pickedFile!.path)),
+                      fit: BoxFit.cover),
+                )),
+        ],
+      ),
+    );
+  }
+
+  _showBottomeSheet() {
+    return showModalBottomSheet(
+      context: context,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(25),
+        ),
+      ),
+      builder: (context) {
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () => _getCameraimage(),
+              child: const Text('촬영'),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Divider(
+              thickness: 3,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+              onPressed: () => _getPhotoLibraryImage(),
+              child: const Text('갤러리에서 불러오기'),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  _getCameraimage() async {
+    //촬영
+    final pickedFile =
+        await ImagePicker().pickImage(source: ImageSource.camera);
+    if (pickedFile != null) {
+      setState(() {
+        _pickedFile = pickedFile;
+      });
+    } else {
+      if (kDebugMode) {
+        print('이미지 선택 안 함');
+      }
+    }
+  }
+
+  _getPhotoLibraryImage() async {
+    //갤러리에서 사진 불러오기
+    final pickedFile =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        _pickedFile = pickedFile;
+      });
+    } else {
+      if (kDebugMode) {
+        print('이미지 선택 안 함');
+      }
+    }
+  }
+}
