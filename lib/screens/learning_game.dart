b3d88579ea44d08f958b419802e97ec25f2f8ac9
HyeonJeong Lim <87944902+sesam0526@users.noreply.github.com>
2023-11-25 22:12:36 +0900

    import 수정

--- lib/screens/learning_game.dart
+++ lib/screens/learning_game.dart
@@ -1,9 +1,5 @@
-import 'dart:math';
 import 'dart:async';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'vocabulary_service.dart';
 import 'package:flutter_project/utils/game_utils.dart';
 
 class LearningGame extends StatefulWidget {
