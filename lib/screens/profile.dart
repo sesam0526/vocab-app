5a84860f2a4af17a099629892d040cfeeac2b702
chaeju <cown4288@gmail.com>
2023-11-29 07:31:06 +0900

    기본 이미지 변경

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,177 +1,9 @@
-/*import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import 'dart:io';
-
-class Profile extends StatefulWidget {
-  const Profile({Key? key}) : super(key: key);
-
-  @override
-  _ProfileState createState() => _ProfileState();
-}
-
-class _ProfileState extends State<Profile> {
-  XFile? _pickedFile;
-
-  @override
-  Widget build(BuildContext context) {
-    final imageSize = MediaQuery.of(context).size.width / 4;
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('프로필'),
-        backgroundColor: Colors.purple[400],
-      ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 20,
-          ),
-          if (_pickedFile == null)
-            Container(
-              constraints: BoxConstraints(
-                minHeight: imageSize,
-                minWidth: imageSize,
-              ),
-              child: GestureDetector(
-                onTap: () {
-                  _showBottomeSheet();
-                },
-                child: Center(
-                  child: Column(
-                    children: [
-                      Icon(
-                        Icons.account_circle,
-                        size: imageSize,
-                      ),
-                      const SizedBox(height: 20),
-                      nameTextField(),
-                    ],
-                  ),
-                ),
-              ),
-            )
-          else
-            Container(
-              width: imageSize,
-              height: imageSize,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(
-                    width: 2, color: Theme.of(context).colorScheme.primary),
-                image: DecorationImage(
-                    image: FileImage(File(_pickedFile!.path)),
-                    fit: BoxFit.cover),
-              ),
-            ),
-        ],
-      ),
-    );
-  }
-
-  Widget nameTextField() {
-    return TextFormField(
-      decoration: InputDecoration(
-          border: OutlineInputBorder(
-            borderSide: BorderSide(
-              color: Colors.black,
-            ),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(
-              color: Colors.green,
-              width: 2,
-            ),
-          ),
-          prefixIcon: Icon(
-            Icons.person,
-            color: Colors.black,
-          ),
-          labelText: '이름',
-          hintText: '이름을 입력하시오'),
-    );
-  }
-
-  _showBottomeSheet() {
-    return showModalBottomSheet(
-      context: context,
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(
-          top: Radius.circular(25),
-        ),
-      ),
-      builder: (context) {
-        return Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-              onPressed: () => _getCameraimage(),
-              child: const Text('촬영'),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Divider(
-              thickness: 3,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            ElevatedButton(
-              onPressed: () => _getPhotoLibraryImage(),
-              child: const Text('갤러리에서 불러오기'),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  _getCameraimage() async {
-    //촬영
-    final pickedFile =
-        await ImagePicker().pickImage(source: ImageSource.camera);
-    if (pickedFile != null) {
-      setState(() {
-        _pickedFile = pickedFile;
-      });
-    } else {
-      if (kDebugMode) {
-        print('이미지 선택 안 함');
-      }
-    }
-  }
-
-  _getPhotoLibraryImage() async {
-    //갤러리에서 사진 불러오기
-    final pickedFile =
-        await ImagePicker().pickImage(source: ImageSource.gallery);
-    if (pickedFile != null) {
-      setState(() {
-        _pickedFile = pickedFile;
-      });
-    } else {
-      if (kDebugMode) {
-        print('이미지 선택 안 함');
-      }
-    }
-  }
-}
-*/
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 
 class Profile extends StatefulWidget {
   const Profile({Key? key}) : super(key: key);
@@ -198,9 +30,7 @@ class _ProfileState extends State<Profile> {
       ),
       body: Column(
         children: [
-          const SizedBox(
-            height: 20,
-          ),
+          //사진을 선택하지 않았을때
           if (_pickedFile == null)
             Container(
               constraints: BoxConstraints(
@@ -214,13 +44,11 @@ class _ProfileState extends State<Profile> {
                 child: Center(
                   child: Column(
                     children: [
-                      Icon(
-                        Icons.account_circle,
-                        size: imageSize,
-                      ),
+                      //기본 이미지 지정
+                      Image.asset('assets/images/mufin1.jpg',
+                          width: imageSize, height: imageSize),
                       const SizedBox(height: 20),
-                      nameTextField(),
-                      const SizedBox(height: 20),
+
                       // 수정: 사용자 정보 출력
                       FutureBuilder<Map<String, dynamic>>(
                         future: _getUserInfo(),
@@ -235,9 +63,11 @@ class _ProfileState extends State<Profile> {
                             return Column(
                               children: [
                                 Text(
-                                  'score: ${snapshot.data!['score']}, '
-                                  'money: ${snapshot.data!['money']}, '
+                                  'nickname: ${snapshot.data!['nickname'] ?? 'DefaultNickname'}\n'
+                                  'score: ${snapshot.data!['score']}\n'
+                                  'money: ${snapshot.data!['money']}\n'
                                   '순위: ${snapshot.data!['rank']}등',
+                                  style: TextStyle(fontSize: 20),
                                 ),
                                 // 여기에 추가적인 정보를 출력할 수 있습니다.
                               ],
@@ -259,8 +89,9 @@ class _ProfileState extends State<Profile> {
                 border: Border.all(
                     width: 2, color: Theme.of(context).colorScheme.primary),
                 image: DecorationImage(
-                    image: FileImage(File(_pickedFile!.path)),
-                    fit: BoxFit.cover),
+                  image: FileImage(File(_pickedFile!.path)),
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
         ],
@@ -280,6 +111,7 @@ class _ProfileState extends State<Profile> {
 
       // 사용자 정보를 Map으로 반환
       return {
+        'nickname': userSnapshot['nickname'],
         'score': userSnapshot['score'],
         'money': userSnapshot['money'],
         'rank': userSnapshot['rank'],
@@ -288,34 +120,12 @@ class _ProfileState extends State<Profile> {
     } catch (e) {
       print('Eror getting user info: &e');
       return {
+        'nicknmae': 'default', // 에러 시 기본값 설정
         'score': 0,
         'money': 0,
         'rank': 0,
       };
     }
-  }
-
-  Widget nameTextField() {
-    return TextFormField(
-      decoration: const InputDecoration(
-          border: OutlineInputBorder(
-            borderSide: BorderSide(
-              color: Colors.black,
-            ),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(
-              color: Colors.green,
-              width: 2,
-            ),
-          ),
-          prefixIcon: Icon(
-            Icons.person,
-            color: Colors.black,
-          ),
-          labelText: '이름',
-          hintText: '이름을 입력하시오'),
-    );
   }
 
   _showBottomeSheet() {
