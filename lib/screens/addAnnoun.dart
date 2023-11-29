6abc70d7f089ac97f23f267be96aab8114223929
naaaaaame <qkrgmldms01@gmail.com>
2023-11-29 23:44:02 +0900

    공지사항

--- lib/screens/addAnnoun.dart
+++ lib/screens/addAnnoun.dart
@@ -2,9 +2,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-
 class AddAnnounScreen extends StatefulWidget {
-  //공지사항 추가 창
+  // 공지사항 추가 창
   const AddAnnounScreen({Key? key}) : super(key: key);
 
   @override
@@ -13,90 +12,99 @@ class AddAnnounScreen extends StatefulWidget {
 
 class _AddAnnounScreen extends State<AddAnnounScreen> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
-   final TextEditingController _titleController = TextEditingController();
-   final TextEditingController _textController = TextEditingController();
-  String mainText='';
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _textController = TextEditingController();
+  String mainText = '';
 
   @override
   Widget build(BuildContext context) {
-  User? currentUser = _auth.currentUser;
- 
- return Scaffold(
+    User? currentUser = _auth.currentUser;
+
+    return Scaffold(
       appBar: AppBar(
         title: const Text('공지사항 추가'),
         backgroundColor: Colors.purple[400],
-      ), 
-       body: SingleChildScrollView(
-         
-        child: Center(
-          child: Column(
-            children: [
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
             TextField(
               controller: _titleController,
               decoration: const InputDecoration(
                 labelText: '공지사항 타이틀',
+                border: OutlineInputBorder(),
               ),
             ),
-            const Padding(padding: EdgeInsets.all(5.0)),
-            SizedBox(
-                  width: double.infinity,
-                  child: Flexible(
+            const SizedBox(height: 14.0),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SingleChildScrollView(
                     child: TextField(
                       maxLines: null,
-              controller: _textController,
-              decoration: const InputDecoration(
-                labelText: '공지사항 내용',
+                      controller: _textController,
+                      decoration: const InputDecoration(
+                        labelText: '공지사항 내용',
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ),
               ),
-              
             ),
-                  ), //TextField 크기
-                ),               
           ],
-          ),
-
-          
-          
-         
         ),
-         
-        ),
-        floatingActionButton: FloatingActionButton.extended(
+      ),
+      floatingActionButton: FloatingActionButton.extended(
         onPressed: () async {
-          //공지사항 저장하고 창 나가기
-        if (_textController.text != ''&& _titleController.text!=''){
-            uploadAnnoun( _titleController.text,  _textController.text);
-          _titleController.clear();
-          _textController.clear();
-          Navigator.of(context).pop();
-          ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(
-            content: Text('공지사항을 업로드하였습니다.'),
-          ),
-        );
-        }else{
-          ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(
-            content: Text('빈 칸으로 업로드할 수 없습니다.'),
-          ),
-        );
-        }
-         
+          // 공지사항 저장하고 창 나가기
+          if (_textController.text.isNotEmpty &&
+              _titleController.text.isNotEmpty) {
+            uploadAnnoun(_titleController.text, _textController.text);
+            _titleController.clear();
+            _textController.clear();
+            Navigator.of(context).pop();
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text('공지사항을 업로드하였습니다.'),
+              ),
+            );
+          } else {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text('빈 칸으로 업로드할 수 없습니다.'),
+              ),
+            );
+          }
         },
         label: const Text(
           '업로드',
           style: TextStyle(fontSize: 20),
         ),
         icon: const Icon(Icons.add),
-        )
+      ),
     );
   }
 
   Future<void> uploadAnnoun(String name, String text) async {
-    //텍스트필드에 적은 내용을 DB에 저장
+    // 현재 날짜와 시간에 대한 타임스탬프 생성
+    DateTime now = DateTime.now();
+    Timestamp timestamp = Timestamp.fromDate(now);
+    // 텍스트필드에 적은 내용을 DB에 저장
     await FirebaseFirestore.instance
-        .collection('Announcement').doc(DateTime.now().toString()).set({'title':name,'main text':text});
-    
-  } 
-
-  
+        .collection('Announcement')
+        .doc(now.toString())
+        .set({
+      'title': name,
+      'main text': text,
+      'timestamp': timestamp,
+    });
+  }
 }
--- lib/screens/announcement.dart
+++ lib/screens/announcement.dart
@@ -1,11 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'announ_detail.dart';
 import 'addAnnoun.dart';
 
 class AnnouncementScreen extends StatefulWidget {
-  //공지사항 창
+  // 공지사항 창
   const AnnouncementScreen({Key? key}) : super(key: key);
 
   @override
@@ -14,75 +15,115 @@ class AnnouncementScreen extends StatefulWidget {
 
 class _AnnouncementScreen extends State<AnnouncementScreen> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  String email='';
+  String email = '';
+
   @override
   Widget build(BuildContext context) {
-  User? currentUser = _auth.currentUser;
-  if(currentUser != null){
-    email=currentUser.email.toString();
-  }
-  
- return Scaffold(
+    User? currentUser = _auth.currentUser;
+    if (currentUser != null) {
+      email = currentUser.email.toString();
+    }
+
+    return Scaffold(
       appBar: AppBar(
         title: const Text('공지사항'),
         backgroundColor: Colors.purple[400],
-         //사용자가 관리자인 계정(master) 일 때만 뜨는 아이콘, 
+        // 사용자가 관리자인 계정(master) 일 때만 뜨는 아이콘,
         actions: [
-         if(email.compareTo('master@gmail.com')==0)
+          if (email.compareTo('master@gmail.com') == 0)
             IconButton(
-            onPressed: () async {
-              //공지사항을 등록하는 창으로 이동하는 함수
-               Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const AddAnnounScreen( )));
-            },
-            icon: const Icon(Icons.add_box,color: Colors.white,),
-          )
-          
-          
+              onPressed: () async {
+                // 공지사항을 등록하는 창으로 이동하는 함수
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const AddAnnounScreen()));
+              },
+              icon: const Icon(Icons.add_box, color: Colors.white),
+            )
         ],
-      ), //DB에 저장된 공지사항의 title 리스트
-       body: StreamBuilder<QuerySnapshot>(
-        stream:announList(),
+      ),
+      // DB에 저장된 공지사항의 title 리스트
+      body: StreamBuilder<QuerySnapshot>(
+        stream: announList(),
         builder: (context, snapshot) {
-          //로딩 중
+          // 로딩 중
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
           }
-          //데이터 로드 시 오류 발생시
+          // 데이터 로드 시 오류 발생시
           if (snapshot.hasError) {
             return const Center(child: Text('데이터 로드 중 오류가 발생했습니다.'));
           }
-          //공지사항이 없을 경우
+          // 공지사항이 없을 경우
           if (snapshot.hasData && snapshot.data!.docs.isEmpty) {
             return const Center(child: Text('공지사항이 없습니다.'));
           }
           var announs = snapshot.data!.docs;
-          return ListView.builder(
-            itemCount: announs.length,
-            itemBuilder: (context, index) {
-              var announcement = announs[index];
-              return ListTile(
-                title: Text(announcement['title']),
-                onTap: () {
-                  Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => AnnounDetailScreen(
-                    announId: announcement.id,
-                    announName: announcement['title'],
-                    )));
-              },
-               // 
-              );
-            },
+          return Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  itemCount:
+                      announs.length * 2, // Include dividers for all items
+                  itemBuilder: (context, index) {
+                    if (index.isOdd) {
+                      // Odd indices correspond to dividers
+                      return const Divider(
+                        color: Colors.grey,
+                        height: 1,
+                      );
+                    }
+
+                    var announcement =
+                        announs[index ~/ 2]; // Adjust index for dividers
+
+                    // Extract the upload date from the document ID
+                    DateTime uploadDate = DateTime.parse(announcement.id);
+
+                    return ListTile(
+                      title: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(announcement['title']),
+                          Text(
+                            DateFormat('yyyy-MM-dd HH:mm:ss')
+                                .format(uploadDate),
+                            style: const TextStyle(
+                                fontSize: 12, color: Colors.grey),
+                          ),
+                        ],
+                      ),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => AnnounDetailScreen(
+                              announId: announcement.id,
+                              announName: announcement['title'],
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
+              ),
+              const Divider(
+                color: Colors.grey,
+                height: 1,
+              ),
+            ],
           );
         },
       ),
     );
   }
 
-  Stream<QuerySnapshot> announList(){
-    //DB에 저장되어있는 공지사항 정보를 받아오는 함수
+  Stream<QuerySnapshot> announList() {
     return FirebaseFirestore.instance
         .collection('Announcement')
+        .orderBy('timestamp', descending: true)
         .snapshots();
   }
 }
