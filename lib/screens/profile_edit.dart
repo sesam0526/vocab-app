339d15feb9bb3d479aa2617e71ba43639623a85c
chaeju <cown4288@gmail.com>
2023-12-04 00:25:44 +0900

    닉네임 수정 오류

--- lib/screens/profile_edit.dart
+++ lib/screens/profile_edit.dart
@@ -9,14 +9,8 @@ class profile_edit extends StatefulWidget {
 }
 
 class _profile_editState extends State<profile_edit> {
-  //late TextEditingController _nicknameController;
   final TextEditingController _nicknameController = TextEditingController();
   String userId = FirebaseAuth.instance.currentUser!.email.toString();
-
-  @override
-  void initState() {
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,14 +25,12 @@ class _profile_editState extends State<profile_edit> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              // User ID를 나타낸다.
               Text(
                 'ID: $userId',
                 style:
                     const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
               const SizedBox(height: 16.0),
-              // 닉네임 수정
               TextField(
                 controller: _nicknameController,
                 decoration: const InputDecoration(labelText: 'nickname'),
@@ -48,16 +40,15 @@ class _profile_editState extends State<profile_edit> {
                 width: double.infinity,
                 child: ElevatedButton.icon(
                   onPressed: () async {
-                    setState(() {
+                    if (mounted) {
                       //  Firestore에 사용자 정보를 업데이트하는 메서드 호출
-                     _updateUserInfo();
-                    // 이전 화면인 profile로 돌아간다.
-                    Navigator.pop(context);
-                    });
-                    
+                      await _updateUserInfo();
+                      // 이전 화면인 profile로 돌아간다.
+                      Navigator.pop(context);
+                    }
                   },
-                  icon: const Icon(Icons.done), // 아이콘 설정
-                  label: const Text(''), // 빈 텍스트 설정 또는 label을 제거
+                  icon: const Icon(Icons.done),
+                  label: const Text(''),
                 ),
               ),
             ],
@@ -69,38 +60,30 @@ class _profile_editState extends State<profile_edit> {
 
   Future<void> _updateUserInfo() async {
     try {
-      // 닉네임 컨트롤러의 값을 가져옴
       String newNickname = _nicknameController.text;
 
-      // 닉네임이 비어 있으면 Firestore에서 이전 닉네임을 가져옴
       if (newNickname.isEmpty) {
-        // 이전 닉네임 가져오기
         DocumentSnapshot userDoc = await FirebaseFirestore.instance
             .collection('users')
             .doc(userId)
             .get();
 
-        // 이전 닉네임이 있는지 확인 후 설정
         if (userDoc.exists) {
           newNickname = userDoc['nickname'];
         } else {
-          // 이전 닉네임이 없으면 기본값 설정 (원하는 값으로 변경)
           newNickname = '';
         }
       }
 
-      // Firestore에서 사용자 정보 업데이트
       await FirebaseFirestore.instance
           .collection('users')
           .doc(userId)
           .update({'nickname': newNickname});
 
-      // 성공 메시지 표시 또는 성공에 따라 처리
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('닉네임이 성공적으로 수정되었습니다.')),
       );
     } catch (e) {
-      // 오류 처리 및 오류 메시지 표시
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('닉네임 수정 중 오류가 발생하였습니다: $e')),
       );
